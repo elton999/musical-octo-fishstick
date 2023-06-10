099f8ef1c36eb9c7395d33cf0e437db72e9a6cb6
Elton Silva <eltoncarsilva73@gmail.com>
2023-06-10 12:33:07 -0300

    dot product on node settings

--- Project/Gameplay/Nodes.cs
+++ Project/Gameplay/Nodes.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using UmbrellaToolsKit;
@@ -8,6 +10,9 @@ namespace Project.Gameplay
     public class Nodes : GameObject
     {
         private Square _square;
+
+        public List<NodePosition> NodesPosition;
+
         public override void Start()
         {
             _square = new Square();
@@ -16,6 +21,27 @@ namespace Project.Gameplay
             _square.size = new Point(8, 8);
             _square.Origin = new Vector2(4, 4);
             _square.Start();
+
+            SetNodePositions();
+        }
+
+        public void SetNodePositions()
+        {
+            NodesPosition = new List<NodePosition>();
+
+            var lastPosition = Position;
+            foreach (var nodePosition in Nodes)
+            {
+                Vector2 toPositionHorizontal = lastPosition * Vector2.UnitX - nodePosition * Vector2.UnitX;
+                Vector2 toPositionVertical = lastPosition * Vector2.UnitY - nodePosition * Vector2.UnitY;
+
+                float horizontal = Vector2.Dot(Vector2.UnitX, toPositionHorizontal);
+                horizontal = MathF.Sign(horizontal);
+                float vertical = Vector2.Dot(Vector2.UnitY, toPositionVertical);
+                vertical = MathF.Sign(vertical);
+
+                lastPosition = nodePosition;
+            }
         }
 
         public override void Draw(SpriteBatch spriteBatch)
@@ -28,5 +54,16 @@ namespace Project.Gameplay
             }
             _square.EndDraw(spriteBatch);
         }
+    }
+
+    public class NodePosition
+    {
+        public Vector2 Position;
+
+        public NodePosition UpPosition;
+        public NodePosition DownPosition;
+
+        public NodePosition LeftPosition;
+        public NodePosition RightPosition;
     }
 }
