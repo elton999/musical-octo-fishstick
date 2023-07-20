887c201d2e8657b4a45cbdef707da252ed125031
Elton Silva <eltoncarsilva73@gmail.com>
2023-07-20 19:58:30 -0300

    levels sprites update

--- Project/Gameplay/Ladder.cs
+++ Project/Gameplay/Ladder.cs
@@ -8,18 +8,11 @@ namespace Project.Gameplay
 {
     public class Ladder : Actor
     {
-        private UmbrellaToolsKit.Sprite.Square _square;
 
         public override void Start()
         {
             base.Start();
             Gravity2D = Vector2.Zero;
-
-            _square = new UmbrellaToolsKit.Sprite.Square();
-            _square.SquareColor = Color.Green;
-            _square.size = size;
-            _square.Position = Position;
-            Scene.AddGameObject(_square);
 
             var platform = new Gameplay.LadderPlatform();
             platform.size = new Point(size.X, 1);
