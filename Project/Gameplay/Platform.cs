e99cdb0d9f0f3ce9bc932c87af64db92ac38016e
Elton Silva <eltoncarsilva73@gmail.com>
2023-08-07 20:18:17 -0300

    fix plataform

--- Project/Gameplay/Platform.cs
+++ Project/Gameplay/Platform.cs
@@ -6,11 +6,17 @@ namespace Project.Gameplay
 {
     public class Platform : Solid
     {
+        public override void Start()
+        {
+            base.Start();
+            Position += Vector2.UnitY;
+        }
+
         public override bool check(Point size, Vector2 position, Actor actor = null)
         {
             if (actor != null && actor.Velocity.Length() > 0 && MathF.Sign(actor.Velocity.Y) == -1)
                 return false;
-            return base.check(size, position);
+            return base.check(size, position + Vector2.UnitY) && !base.check(size, position);
         }
     }
 }
